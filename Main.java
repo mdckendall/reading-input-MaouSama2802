a5353a016dd6a550a387d9ac2a8022e12486d42a
MaouSama2802 <132946958+MaouSama2802@users.noreply.github.com>
2023-06-21 23:11:26 +0000

    .length()

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
-    
+    Scanner in = new Scanner(System.in);
+    String word =in.nextLine();
+   System.out.println(word.length());
     //Your code will return the length of the entered String
   }
 }
